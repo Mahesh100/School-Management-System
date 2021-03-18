e6cbd777117abe483d438df931985ade221d002b
Mahesh Kakde <maheshkakde100@gmail.com>
2021-03-18 23:52:09 +0530

    Update School.java

---
+++ School.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+/**
+ * Many teachers, many students.
+ * Implements teachers and student using an Array list
+ * Created by Mahesh on 18/03/2021
+ */
+public class School {
+
+
+    private List<Teacher> teachers;
+    private Lisr<Student> students;
+    private int totalMoneyEarned;
+    private int totalMoneySpetnt;
+    
+}
