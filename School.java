c16bdba9d6c540f4e640ab2bb49d5e985e7ac82a
Mahesh Kakde <maheshkakde100@gmail.com>
2021-03-19 00:00:18 +0530

    Update School.java

--- School.java
+++ School.java
@@ -12,5 +12,20 @@ public class School {
     private Lisr<Student> students;
     private int totalMoneyEarned;
     private int totalMoneySpetnt;
+
+
+    /**
+     * new school obejct is created.
+     * /@ param teacher list of teachers in the school
+     * /@ param students list of students in the school.
+     */
+    public School(List<Teacher> teachers, Lisr<Students> students){
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned=0;
+        totalMoneySpetnt=0;
+    }
     
+     public List<
+
 }
