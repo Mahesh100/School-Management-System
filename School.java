a2ec99846c824c426099f8e4974f3f4d66fa18ce
Mahesh Kakde <maheshkakde100@gmail.com>
2021-03-19 23:13:40 +0530

    Update School.java

--- School.java
+++ School.java
@@ -9,8 +9,8 @@ public class School {
 
 
     private List<Teacher> teachers;
-    private Lisr<Student> students;
-    private int totalMoneyEarned;
+    private List<Students> students;
+    private int TotalMoneyEarned;
     private int totalMoneySpetnt;
 
 
@@ -19,13 +19,87 @@ public class School {
      * /@ param teacher list of teachers in the school
      * /@ param students list of students in the school.
      */
-    public School(List<Teacher> teachers, Lisr<Students> students){
+    public School(List<Teacher> teachers, List<Students> students){
         this.teachers = teachers;
         this.students = students;
         totalMoneyEarned=0;
         totalMoneySpetnt=0;
     }
-    
-     public List<
+    /**
+     * 
+     * /@ return the list of teachers in the school.
+     */
+     public List<Teacher> getTeachers(){
+         return teachers;
+
+     }
+     
+     /**
+      * Adds a teachers to the school.
+      * /@ param teacher the teacher to be added
+      */
+     public void addTeachers(Teacher teacher){
+         teachers.add(teacher);
+     }
+
+
+     /**
+      * 
+      * @return the list of students in the school.
+      */
+     public List<Students> getStudents(){
+         return students;
+     }
+      
+
+     /**
+      * add a student to the school
+      * /@ param student the stident to be added.
+      */
+     public void addStudent(Students student) {
+         students.add(student);
+     }
+
+
+     /**
+      * 
+      * 
+      * /@ return the total money earned by the school.
+      */
+
+     public int getTotalMoneyEarned( int totalMoneyErned){
+         this.totalMoneyEarned = totalMoneyErned;
+     }
+
+
+     /**
+      * Adds the total money earned by the school.
+      *  /@ param MoneyEarned money that is supposed to be added.
+      */
+
+      public void updateTotalMoneyEarned(int MoneyEarned){
+             totalMoneyEarned += MoneyEarned;
+      }
+
+      /**
+       * 
+       * /@ return the total money spent by the school.
+       */
+
+     public int getTotalMoneySpent(){
+         
+         return totalMoneySpent;
+     }
+
+
+   /**
+    * update the money that is spent by the school which the salary given by the school to its teachers.
+    * 
+    * / @ param MoneySpent the money spent by school
+    */
+     public void updateTotalMoneySpent(int MoneySpent){
+         
+         totalMoneyEarned-=MoneySpent;
+     }
 
 }
